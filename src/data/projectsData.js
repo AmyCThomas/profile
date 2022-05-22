bd0dc4aadfda898e9ca530a0c240abde176cc071
Amy Thomas <cofcfourteen@gmail.com>
2022-05-22 14:40:43 -0600

    added project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -6,29 +6,30 @@ import five from '../assets/svg/projects/five.svg'
 import six from '../assets/svg/projects/six.svg'
 import seven from '../assets/svg/projects/seven.svg'
 import eight from '../assets/svg/projects/eight.svg'
+import { AirplanemodeActive, ImageSearch, NextWeek, OpenInNewSharp } from '@material-ui/icons'
 
 
 export const projectsData = [
+
     {
         id: 1,
-        projectName: '',
+        projectName: 'WhatToPaint?',
+    projectDesc: 'This project is using "Openai"\'s api to ask for suggestions of what to paint/draw based on a general subject entered.  I started with a python version, then switched to Next.js after having a difficult time deploying it with Netlify.  Though I have the basic model deployed and working, I will be continuing to work on some issues such as wanting the responses on seperate lines, and the fact that it returned the input as well as the prompts in python and at first with next.js, but will no longer do so. I\'d also like photos either from openai\'s new photo based APIs (which I\' awaiting approvel for after requesting access) or something like Getty\'s Images API.',
+        tags: ['HTML', 'CSS','Javascript','Nextjs','next.js','netlify', 'api','python'],
+        code: 'https://github.com/amycthomas/WhattoPaint',
+        demo: 'https://6289c3b3d397810f081c4f38--moonlit-rolypoly-6a3464.netlify.app/o',
+        image: "https://screenshot-proxy.netlify.app/f_avif,w_336/https://d33wubrfki0l68.cloudfront.net/628a99b38372441a0f7a7617/screenshot_2022-05-22-20-15-47-0000.png"
+    },
+
+    {
+        id: 2,
+        projectName: 'ArtPod (coming very soon - by may-25',
         projectDesc: '',
         tags: ['',''],
         code: '',
         demo: '',
         image: ""
     },
-    {
-        id: 2,
-        projectName: 'Sportsjam.in Clone',
-        projectDesc: 'This is a clone of an e-commerce website sportsjam.in. My contribution to this project was the product catalogue page and its features, like sorting and filtering. The rest of the project was built by various members of the group',
-        tags: ['HTML', 'CSS','Javascript'],
-        code: 'https://github.com/sreerag-rajan/sportsjam-clone',
-        demo: 'https://practical-meninsky-36f305.netlify.app/',
-        image: "https://camo.githubusercontent.com/f9fbe30b577cff2fbf09af53fb3c1f12fcc10a771bf9459ed5af10bffab9e38b/68747470733a2f2f6d69726f2e6d656469756d2e636f6d2f6d61782f3837352f312a525f5f63646e384c316759474b3154647171797562672e706e67"
-    },
-
-    
 ]
 
 
