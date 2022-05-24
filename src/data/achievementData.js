a555907c9f61fe9bfebe3f463bcaa562c656f68a
Amy Thomas <cofcfourteen@gmail.com>
2022-05-24 15:46:18 -0600

    edits

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,28 +1,28 @@
-// export const achievementData = {
-//     bio : "",
-//     achievements : [
-//         {
-//             id : 1,
-//             title : '',
-//             details : '',
-//             date : '',
-//             field : '',
-//             image : ''
-//         },
+export const achievementData = {
+    bio : "",
+    achievements : [
+        {
+            id : 1,
+            title : '',
+            details : '',
+            date : '',
+            field : '',
+            image : ''
+        },
         
 
-//     ]
-// }
+    ]
+}
 
 
-// // Do not remove any fields.
-// // Leave it blank instead as shown below.
+// Do not remove any fields.
+// Leave it blank instead as shown below.
 
-// /*
+/*
 
-// export const achievementData = {
-//     bio : "",
-//     achievements : []
-// }
+export const achievementData = {
+    bio : "",
+    achievements : []
+}
 
-// */
+*/
--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,10 +1,20 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Mturkn',
+        company: 'Mturk',
         jobtitle: 'Contracter',
         startYear: '2015',
         endYear: '2021'
+    },
+    
+]
+export const experienceData = [
+    {
+        id: 2,
+        company: 'At&T',
+        jobtitle: 'Cutomer Service, Tech Support',
+        startYear: '2012',
+        endYear: '2023'
     },
     
 ]
